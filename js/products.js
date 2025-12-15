a627d37659e2f0e60856d82e637a6dd2829aadb5
EshwarNaikG <eshwarnaik79@gmail.com>
2025-12-15 15:58:53 +0530

    third coment

--- js/products.js
+++ js/products.js
@@ -8,5 +8,35 @@ const products = [
   { id: 1, name: "Laptop", price: 800, image: "images/laptop.png" },
   { id: 2, name: "Phone", price: 500, image: "images/phone.png" },
   { id: 3, name: "Headphones", price: 150, image: "images/headphones.png" },
-  { id: 4, name: "Smart Watch", price: 200, image: "images/watch.png" }
+  { id: 4, name: "Smart Watch", price: 200, image: "images/watch.png" },
+
+   { id: 1, name: "Laptop", price: 800, image: "images/laptop.png" },
+  { id: 2, name: "Phone", price: 500, image: "images/phone.png" },
+  { id: 3, name: "Headphones", price: 150, image: "images/headphones.png" },
+  { id: 4, name: "Smart Watch", price: 200, image: "images/watch.png" },
+
+   { id: 1, name: "Laptop", price: 800, image: "images/laptop.png" },
+  { id: 2, name: "Phone", price: 500, image: "images/phone.png" },
+  { id: 3, name: "Headphones", price: 150, image: "images/headphones.png" },
+  { id: 4, name: "Smart Watch", price: 200, image: "images/watch.png" },
+
+   { id: 1, name: "Laptop", price: 800, image: "images/laptop.png" },
+  { id: 2, name: "Phone", price: 500, image: "images/phone.png" },
+  { id: 3, name: "Headphones", price: 150, image: "images/headphones.png" },
+  { id: 4, name: "Smart Watch", price: 200, image: "images/watch.png" },
+
+   { id: 1, name: "Laptop", price: 800, image: "images/laptop.png" },
+  { id: 2, name: "Phone", price: 500, image: "images/phone.png" },
+  { id: 3, name: "Headphones", price: 150, image: "images/headphones.png" },
+  { id: 4, name: "Smart Watch", price: 200, image: "images/watch.png" },
+
+   { id: 1, name: "Laptop", price: 800, image: "images/laptop.png" },
+  { id: 2, name: "Phone", price: 500, image: "images/phone.png" },
+  { id: 3, name: "Headphones", price: 150, image: "images/headphones.png" },
+  { id: 4, name: "Smart Watch", price: 200, image: "images/watch.png" },
+
+   { id: 1, name: "Laptop", price: 800, image: "images/laptop.png" },
+  { id: 2, name: "Phone", price: 500, image: "images/phone.png" },
+  { id: 3, name: "Headphones", price: 150, image: "images/headphones.png" },
+  { id: 4, name: "Smart Watch", price: 200, image: "images/watch.png" },
 ];
